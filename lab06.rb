0c522865e7c31ed274b9efa2fca0496eed503d74
TxMoose <kyle@brieden.org>
2013-10-30 09:12:50 +0000

    Began lab 6

---
+++ lab06.rb
@@ -0,0 +1,17 @@
+def addition(x, y)
+  return x + y
+end
+
+def subtraction(x, y)
+  return x - y
+end
+
+def multiplication(x, y)
+  return x * y
+end
+
+def division(x, y)
+  return x / y
+end
+
+
